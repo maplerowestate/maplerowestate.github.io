abb39b14138468f7d4987ad79a268302f09e869e
hbonds <hamilton.t.bonds.mil@mail.mil>
2025-02-18 18:16:13 -0500

    2025-02-18:@maplerowestate:fixed broken gh pages deployment, hopefully.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/maple-row-estate/', // Replace with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3000,
   },
 })
